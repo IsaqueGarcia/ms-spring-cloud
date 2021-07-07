36a1b04913041bd3a058ed3c9e0a83642e6be583
IsaqueGarcia <isaqueemprego1@gmail.com>
2021-07-07 12:17:04 -0300

    First commit

---
+++ hr-worker/src/test/java/com/isaquespring/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaquespring.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
