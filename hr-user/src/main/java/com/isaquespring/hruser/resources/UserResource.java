b5515ebdd8b70c3e5aadc23ab050a5634a657815
Isaque da Silva Garcia <isaque.garcia@orbitallcorp.com.br>
2022-02-16 20:55:49 -0300

    UserRepository, UserResource, Zuul config

---
+++ hr-user/src/main/java/com/isaquespring/hruser/resources/UserResource.java
@@ -0,0 +1,37 @@
+package com.isaquespring.hruser.resources;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.isaquespring.hruser.entities.User;
+import com.isaquespring.hruser.repositories.UserRepository;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	private static Logger logger = org.slf4j.LoggerFactory.getLogger(UserResource.class);
+	
+	//Injetar de forma automatica, proprio do spring
+	@Autowired
+	private UserRepository repository;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable("id") Long id){
+		User user = repository.findById(id).get();
+		return ResponseEntity.ok(user);
+	}
+	
+	@GetMapping(value = "/search")
+	public ResponseEntity<User> findById(@RequestParam String email){
+		User user = repository.findByEmail(email);
+		return ResponseEntity.ok(user);
+	}
+	
+}
